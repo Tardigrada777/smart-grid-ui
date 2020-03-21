28d176a81347323e6ae5d313a2398eb0b979d461
tardigrada777 <ntwrite@gmail.com>
2020-03-21 13:35:18 +0300

    feat: bootstrap electron-vue project structure

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'smart-grid-ui'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
