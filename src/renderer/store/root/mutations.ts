ee02e2010a04934f745a0abcaba8afa77dd60cee
tardigrada777 <ntwrite@gmail.com>
2020-03-22 20:23:52 +0300

    fix(docs): fix width of screenshots in README

--- src/renderer/store/root/mutations.ts
+++ src/renderer/store/root/mutations.ts
@@ -2,21 +2,5 @@ import { MutationTree } from 'vuex';
 import RootState from './state';
 
 
-/**
- * Name of setLocale mutation.
- */
-export const SET_LOCALE: string = 'setLocale';
-
-/**
- * Sets new locale to store.
- *
- * @param state     Root state of vuex store.
- * @param locale    New value of locale.
- */
-export function setLocale(state: RootState, locale: string) {
-    state.locale = locale;
-}
-
 export default {
-    setLocale
 } as MutationTree<RootState>
--- src/renderer/store/root/state.ts
+++ src/renderer/store/root/state.ts
@@ -4,14 +4,7 @@
 export default class RootState {
 
     /**
-     * Application locale.
-     */
-    public locale: string;
-
-    /**
      * Creates initial root store state.
      */
-    constructor() {
-        this.locale = 'en';
-    }
+    constructor() {}
 }
